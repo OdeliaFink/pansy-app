8935b793110ce525e7ba0b478f3839f2bf9c40f5
OdeliaFink <odelia.finkel@gmail.com>
2023-12-04 10:58:33 -0500

    refactor

--- src/app/[lang]/[uid]/page.js
+++ src/app/[lang]/[uid]/page.js
@@ -1,18 +1,18 @@
-import { SliceZone } from "@prismicio/react";
-import * as prismic from "@prismicio/client";
+import { SliceZone } from '@prismicio/react';
+import * as prismic from '@prismicio/client';
 
-import { getLocales } from "@/lib/getLocales";
-import { createClient } from "@/prismicio";
+import { getLocales } from '@/lib/getLocales';
+import { createClient } from '@/prismicio';
 
-import { Layout } from "@/components/Layout";
-import { components } from "@/slices";
+import { Layout } from '@/components/Layout';
+import { components } from '@/slices';
 
 /**
  * @returns {Promise<import("next").Metadata>}
  */
 export async function generateMetadata({ params: { uid, lang } }) {
   const client = createClient();
-  const page = await client.getByUID("page", uid, { lang });
+  const page = await client.getByUID('page', uid, { lang });
 
   return {
     title: prismic.asText(page.data.title),
@@ -22,9 +22,9 @@ export async function generateMetadata({ params: { uid, lang } }) {
 export default async function Page({ params: { uid, lang } }) {
   const client = createClient();
 
-  const page = await client.getByUID("page", uid, { lang });
-  const navigation = await client.getSingle("navigation", { lang });
-  const settings = await client.getSingle("settings", { lang });
+  const page = await client.getByUID('page', uid, { lang });
+  const navigation = await client.getSingle('navigation', { lang });
+  const settings = await client.getSingle('settings', { lang });
 
   const locales = await getLocales(page, client);
 
@@ -35,12 +35,12 @@ export default async function Page({ params: { uid, lang } }) {
   );
 }
 
-export default async function generateStaticParams() {
+export async function generateStaticParams() {
   const client = createClient();
 
-  const pages = await client.getAllByType("page", {
-    lang: "*",
-    filters: [prismic.filter.not("my.page.uid", "home")],
+  const pages = await client.getAllByType('page', {
+    lang: '*',
+    filters: [prismic.filter.not('my.page.uid', 'home')],
   });
 
   return pages.map((page) => {
