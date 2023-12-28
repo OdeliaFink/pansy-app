2eaa6da570d3ca49d3af62c96d1e569955ef4f42
OdeliaFink <odelia.finkel@gmail.com>
2023-12-28 15:23:35 -0500

    refactor

--- src/slices/Values/index.js
+++ src/slices/Values/index.js
@@ -1,23 +1,27 @@
+'use client';
 import { Bounded } from '@/components/Bounded';
 import Flower from '@/components/Flower';
+import { useState } from 'react';
 
-/**
- * @typedef {import("@prismicio/client").Content.ValuesSlice} ValuesSlice
- * @typedef {import("@prismicio/react").SliceComponentProps<ValuesSlice>} ValuesProps
- * @param {ValuesProps}
- */
 const Values = ({ slice }) => {
+  const [activeItem, setActiveItem] = useState(null); // State to track the active item
+
+  // Function to toggle the active state of an item
+  const toggleActive = (index) => {
+    setActiveItem(activeItem === index ? null : index); // Toggle active state for clicked item
+  };
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="bg-medium-beige mb-4"
+      className="bg-medium-beige mb-4 "
     >
-      <div className=" flex md:flex-row flex-col md:items-center">
-        <div className="max-w-[25rem] md:text-left text-center">
+      <div className="flex md:flex-row flex-col md:items-center md:gap-[11rem]">
+        <div className="max-w-[28rem] md:text-left text-center md:pl-[4rem]">
           <div className="flex flex-col justify-center">
-            <Flower />
-            <h1 className="text-[2rem]  font-body font-regular">
+            {/* <Flower /> */}
+            <h1 className="lg:text-[4rem] text-[3rem] font-body font-regular">
               {slice.primary.heading}
             </h1>
           </div>
@@ -25,28 +29,34 @@ const Values = ({ slice }) => {
             {slice.primary.subheading}
           </p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col md:w-full">
           {slice.items.map((item, index) => (
-            <div
-              key={index}
-              className="flex flex-col flex-2 md:w-[60%] md:text-left text-center ml-auto md:mr-[4rem] mr-[0rem]"
-            >
-              {index !== slice.items.length && ( // Add line divider if it's not the last item
-                <div className="border-t my-6 border-gray-300" />
-              )}
-              <div className=" flex flex-col gap-3">
-                <p className="font-regular md:text-[2.3rem] text-[1.5rem] text-mossy-green pb-2 leading-none font-body">
-                  {item.value}
-                </p>
-                <p className="font-light text-[.8rem] md:text-[1rem] md:w-full w-[70%] mx-auto font-body font-light break-words hyphens-auto ">
+            <div key={index} className="flex flex-col md:text-left ml-auto ">
+              <div className="my-6 border-gray-300" />
+              <div className="flex flex-col gap-3 hover:pointer">
+                <div
+                  className="flex flex-row items-center justify-between ease-in-out duration-200 cursor-pointer"
+                  onClick={() => toggleActive(index)} // Toggle active state for clicked item
+                >
+                  <p className="font-regular md:text-[2.3rem] text-[1.5rem] text-mossy-green pb-2 leading-none font-body">
+                    {item.value}
+                  </p>
+                  <div className="font-regular md:text-[2.3rem] text-[1.5rem] text-mossy-green">
+                    {activeItem === index ? '-' : '+'}
+                  </div>
+                </div>
+                <div
+                  className={`font-light text-[.8rem] md:text-[1rem] w-full mx-auto font-body font-light break-words hyphens-auto transition-all ${
+                    activeItem === index
+                      ? 'h-auto opacity-100'
+                      : 'h-0 opacity-0'
+                  }`}
+                >
                   {item.value_desc}
-                </p>
+                </div>
               </div>
             </div>
           ))}
-          {slice.items.length - 1 && ( // Add line divider if it's not the last item
-            <div className="border-t mt-4 mb-4 border-gray-300 md:w-[60%] w-[80%] ml-auto md:mr-[4rem] mr-auto" />
-          )}
         </div>
       </div>
     </Bounded>
