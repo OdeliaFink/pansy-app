a60115acf723bfac25e847d047d31fd15c8b4413
OdeliaFink <odelia.finkel@gmail.com>
2023-12-29 16:12:51 -0500

    refactor

--- src/slices/PhotoGallery/index.js
+++ src/slices/PhotoGallery/index.js
@@ -25,7 +25,7 @@ const PhotoGallery = ({ slice }) => {
       <section
         data-slice-type={slice.slice_type}
         data-slice-variation={slice.variation}
-        className="flex justify-center flex-row flex-wrap gap-8 py-6 px-10"
+        className="flex justify-center flex-row flex-wrap gap-8 py-6 pl-10"
       >
         <div className="pt-10 pb-4 flex md:flex-row flex-col items-center px-10 gap-12">
           <h1 className="font-body text-[3rem] md:text-left leading-none text-center text-slate-grey">
@@ -37,26 +37,56 @@ const PhotoGallery = ({ slice }) => {
           </div>
         </div>
         <Swiper
-          slidesPerView={4}
+          slidesPerView={3.5}
+          breakpoints={{
+            // when window width is >= 320px
+            370: {
+              slidesPerView: 1.5,
+              spaceBetween: 24,
+            },
+            770: {
+              slidesPerView: 3.5,
+            },
+            1000: {
+              slidesPerView: 3.5,
+            },
+          }}
           navigation
-          scrollbar={{ draggable: true }}
+          // scrollbar={{ draggable: true }}
           spaceBetween={50}
         >
-          <SwiperSlide>
-            <PrismicNextImage field={slice.primary.image1} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <PrismicNextImage field={slice.primary.image2} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <PrismicNextImage field={slice.primary.image3} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <PrismicNextImage field={slice.primary.image4} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <PrismicNextImage field={slice.primary.image5} />
-          </SwiperSlide>
+          <div>
+            <SwiperSlide>
+              <PrismicNextImage
+                field={slice.primary.image1}
+                className="rounded-p-md"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PrismicNextImage
+                field={slice.primary.image2}
+                className="rounded-p-md"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PrismicNextImage
+                field={slice.primary.image3}
+                className="rounded-p-md"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PrismicNextImage
+                field={slice.primary.image4}
+                className="rounded-p-md"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PrismicNextImage
+                field={slice.primary.image5}
+                className="rounded-p-md"
+              />
+            </SwiperSlide>
+          </div>
         </Swiper>
       </section>
     </>
