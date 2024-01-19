49135853869769a0c71c9c32abd62ea81872d01b
OdeliaFink <odelia.finkel@gmail.com>
2024-01-19 12:58:26 -0500

    refactor

---
+++ src/components/SwiperNavButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../app/globals.css';
+
+import { useSwiper } from 'swiper/react';
+
+export const SwiperNavButtons = () => {
+  const swiper = useSwiper();
+
+  return (
+    <div className="swiper-nav-btns">
+      <button onClick={() => swiper.slidePrev()}>Prev</button>
+      <button onClick={() => swiper.slideNext()}>Next</button>
+    </div>
+  );
+};
