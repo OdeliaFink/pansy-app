c771b5005d815cac8f7a43a19beb872bca79fb66
Carlos Garate <carlos.m.garate@gmail.com>
2024-02-19 13:47:54 -0500

    add robots to avoid redirect

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,10 @@ import { createClient } from "@/prismicio";
 
 export async function middleware(request) {
   const client = createClient();
-  if (request.nextUrl.pathname.startsWith('/sitemap')) {
+  if (
+    request.nextUrl.pathname.startsWith("/sitemap") ||
+    request.nextUrl.pathname.startsWith("/robots")
+  ) {
     return;
   }
   const redirect = await createLocaleRedirect({ client, request });
